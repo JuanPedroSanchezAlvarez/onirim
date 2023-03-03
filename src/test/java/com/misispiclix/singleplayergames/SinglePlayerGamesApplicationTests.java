598dda4616a3d6a717088145044f679da5f1f989
JuanPedroSanchezAlvarez <juanpedro.sanchez@arenatech.es>
2023-03-03 10:53:54 +0100

    Initial commit.

---
+++ src/test/java/com/misispiclix/singleplayergames/SinglePlayerGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misispiclix.singleplayergames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinglePlayerGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
